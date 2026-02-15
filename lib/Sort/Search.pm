3f82925e2b3e2632d644822a0e1a28d0dbb35123
Ethan Meng <ethan@rapidcow.org>
2026-02-14 21:03:18 -0600

    Transplant AUTHORship and LICENSE to source file
    By the time I upload this on CPAN i'll add a link to
    MetaCPAN as well... but at the moment I don't want to
    put any link that wouldn't work. :x
    
    Also, I personally don't want my name to appear on the
    Web that easily (besides the fact that I am personally
    not a fan of the AUTHOR(S) section in a man page....)
    So i'm moving all the copyright stuff out since, at
    least according to the Linux man page project, that
    stuff belongs to a comment like an SCCS identifier....
    
    (I know MetaCPAN provides a web/HTML view of the source
    itself, but ehhh.... it's relatively more obscure.  And
    obscurity is just what I need, so. =,=)

--- lib/Sort/Search.pm
+++ lib/Sort/Search.pm
@@ -1,3 +1,11 @@
+#
+# Copyright (c) 2025, Ethan Meng <ethan@rapidcow.org>
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the same terms as Perl itself.
+# See LICENSE at the root directory of your distribution
+# for a copy of Perl's licenses.
+#
 package Sort::Search;
 
 our $VERSION = '0.01_00';
