2fa053fd3d81992d34640eef1024f3b8f62bcbc1
Ethan Meng <ethan@rapidcow.org>
2026-03-14 23:01:18 -0500

    Test out implication of the five-element form
    ... of a possible method to compute NSEC records
    on-the-fly?  Though I know it is kind of useless
    without the RRSIG, and if the zone were signed we
    only need to find the NSEC identified by just the
    predecessor, in which case brsrch0 (or even brsrchx)
    alone would suffice...
    
    Nevertheless it is fun to think about how in
    *theory* the additional bounds could be used,
    so... I'll take it as that :P

---
+++ xlib/nsec-search.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+C<xt/nsec-search.pl> -
+Search for predecessor and succesor.
+Inspired by DNSSEC NextSECure (NSEC) records.
+
+Modified from C<blsrch2>.  The funny suffix
+is short for "Previous" and "neXt".
+
+For an example of extending the real C<Search.pm>
+with this, see C<xt/nsec-search.t>.  I don't have
+a good name for this, nor see a good use case for
+it besides the (unrealistic) use case as a DNSSEC
+name server in action, so it will lives in the
+C<xlib/> directory for now.
+
+=cut
+
+package Sort::Search;
+
+=head1 SUBROUTINES
+
+=over
+
+=item B<bsrchpx { ORD } ARRAY>
+
+Like C<blsrch2> (but reversed!), this return the
+last (right) and first (left) match on "success",
+OR the predecessor and the successor on "failure".
+
+(I put "failure" in air quotes because that is
+precisely the NSEC record that would be served
+for when a domain name falls within such range. ;)
+
+The index of each of the two matches, C<$hi>
+and C<$lo>, is collocated among the matches to
+distinguish the case of a "success" and the
+case of "failure".  In the case of the former,
+C<< $hi >= $lo >>; in the case of the latter,
+C<< $hi < $lo >> (in fact, they differ by 1).
+
+=cut
+
+sub _bsrchpx
+{
+	my ($ord, $map, $min) = @_;
+	my ($prv, $nxt);
+	my ($mid, $res, $elp, $lo, $hi) = _blsrch(1, sub { $_[0] >= 0 }, @_);
+
+	if (defined $res && $res == 0) {
+		# $hi has been generously suspended by
+		# "blsrchx" as it thinks we are still
+		# looking for an (exclusive) upper bound
+		# ... which we aren't, so we're good to
+		# exclude it. :)  (This is equivalent to
+		# what they do in traditional binary search
+		# with closed bounds instead of half-open
+		# bounds, by the way.)
+		--$hi;
+		if ($lo == $hi) {
+			# SPECIAL: The search range has almost
+			# collapsed, even though we have found
+			# the (sole) match.  This would be our
+			# common pointer, so return that.
+			$prv = $nxt = $elp;
+		} else {
+			# Search on a possibly long range?
+			($hi, undef, $prv) = _brsrch(0, sub { $_[0] <= 0 }, $ord, $map, $hi, $mid);
+			($lo, undef, $nxt) = _blsrch(0, sub { $_[0] >= 0 }, $ord, $map, $lo, $mid);
+		}
+	} else {
+		# No match found; now the left and right match cross
+		# over one other to become successor and predecessor
+		$nxt = $elp;
+		$prv = --$hi < $min ? undef :
+			defined $map ? $map->($hi) : \$hi;
+	}
+	($hi, defined $prv ? $$prv : undef,
+	 $lo, defined $nxt ? $$nxt : undef);
+}
+
+sub bsrchpx (&$;$$) { _bsrchpx(_parse(LTR, @_)); }
+
+push @EXPORT_OK, qw(bsrchpx);
+
+=item B<cmp_zone A, B>
+
+Implements canonical DNS name order.
+Any IDN is assumed to have been encoded.
+
+In addition, if B<A> is a subdomain of B<B>
+or vice versa, return 1/-1.  This is similar
+to the C<acomp> function in Plan9, but except
+reverse because domain names are written in
+reverse.
+
+=cut
+
+sub cmp_zone
+{
+	my $cmp;
+	# Pointer indeX String Remaining
+	my ($p0, $x0, $s0, $r0) = (length($_[0]) - 1);
+	my ($p1, $x1, $s1, $r1) = (length($_[1]) - 1);
+	# Skip the root zone dot
+	$p0-- if $p0 > 0 && substr($_[0], $p0, 1) eq '.';
+	$p1-- if $p1 > 0 && substr($_[1], $p1, 1) eq '.';
+	do {
+		$r0 = ($x0 = rindex($_[0], ".", $p0)) > 0;
+		$r1 = ($x1 = rindex($_[1], ".", $p1)) > 0;
+		# Compare labels
+		$s0 = lc(substr($_[0], $x0 + 1, $p0 - $x0));
+		$s1 = lc(substr($_[1], $x1 + 1, $p1 - $x1));
+		if ($cmp = ($s0 cmp $s1)) {
+			return 2*$cmp;
+		}
+		$x0-- if $r0;
+		$x1-- if $r1;
+		($p0, $p1) = ($x0, $x1);
+	} while ($r0 && $r1);
+	if ($cmp = ($r0 <=> $r1)) {
+		return $cmp;
+	}
+	return 0;
+}
+
+push @EXPORT_OK, qw(cmp_zone);
+
+=back
+
+=head1 SEE ALSO
+
+L<RFC 4034 "Resource Records for
+the DNS Security Extensions,
+Section 6.1 "Canonical DNS Name Order"
+|https://datatracker.ietf.org/doc/html/rfc4034#section-6.1>
+
+L<Plan 9, /sys/src/cmd/look.c, int acomp(Rune *s, Rune *t)
+|https://github.com/plan9foundation/plan9/blob/main/sys/src/cmd/look.c>
+
+=cut
+
+1;
---
+++ xt/nsec-search.t
@@ -0,0 +1,79 @@
+#!perl
+
+use Sort::Search;
+use Test::More;
+
+BEGIN {
+	unless (eval {
+		require './xlib/nsec-search.pl';
+		Sort::Search->import(qw(bsrchpx cmp_zone));
+		1;
+	}) {
+		note "eval error: $@";
+		plan skip_all => "SKIP: failed to load \`./xlib/nsec-search.pl'";
+	}
+
+	plan tests => 1;
+}
+
+# https://blog.cloudflare.com/black-lies/
+subtest "ietf.org" => sub {
+	plan tests => 15;
+
+	my $ORIGIN = "ietf.org.";
+	chomp (my @mock_zone = map { s/ +/ /g; $_ } split /^/, <<RR);
+$ORIGIN       1  IN  SOA
+$ORIGIN       2  IN  A
+$ORIGIN       3  IN  TXT
+$ORIGIN       4  IN  MX
+$ORIGIN       5  IN  NS
+$ORIGIN       6  IN  AAAA
+ietf1._domainkey.$ORIGIN  1  IN TXT
+apps.$ORIGIN  1  IN  A
+apps.$ORIGIN  2  IN  MX
+apps.$ORIGIN  3  IN  AAAA
+mail.apps.$ORIGIN  1  IN  A
+mail.apps.$ORIGIN  2  IN  AAAA
+www.apps.$ORIGIN   1  IN  A
+www.apps.$ORIGIN   2  IN  AAAA
+cloudflare-verify.$ORIGIN   1  IN  TXT
+RR
+	my $cmp_rr = sub {
+		my ($owner0) = split / /, $_[0];
+		my $owner1 = $_[1];
+		cmp_zone($owner0, $owner1);
+	};
+
+	my ($hi, $prv, $lo, $nxt);
+
+	# This example is given as the NSEC record
+	# that denies the existence of "bogus.ietf.org."
+	($hi, $prv, $lo, $nxt) = bsrchpx
+		{ $cmp_rr->($_, "bogus.$ORIGIN") }
+		\@mock_zone;
+	cmp_ok ($hi, '<', $lo);
+	cmp_ok ($hi, '==', 13);
+	cmp_ok ($lo, '==', 14);
+	is ($prv, "www.apps.$ORIGIN 2 IN AAAA");
+	is ($nxt, "cloudflare-verify.$ORIGIN 1 IN TXT");
+
+	# Similarly for the wildcard domain...
+	($hi, $prv, $lo, $nxt) = bsrchpx
+		{ $cmp_rr->($_, "*.$ORIGIN") }
+		\@mock_zone;
+	cmp_ok ($hi, '<', $lo);
+	cmp_ok ($hi, '==', 5);
+	cmp_ok ($lo, '==', 6);
+	is ($prv, "$ORIGIN 6 IN AAAA");
+	is ($nxt, "ietf1._domainkey.$ORIGIN 1 IN TXT");
+
+	# Domain that has a lot of records
+	($hi, $prv, $lo, $nxt) = bsrchpx
+		{ $cmp_rr->($_, $ORIGIN) }
+		\@mock_zone;
+	cmp_ok ($hi, '>=', $lo);
+	cmp_ok ($hi, '==', 5);
+	cmp_ok ($lo, '==', 0);
+	is ($prv, "$ORIGIN 6 IN AAAA");
+	is ($nxt, "$ORIGIN 1 IN SOA");
+};
